b9f89ba4c4ffd23edba57f3916e4484e93e4f362
sara.melo <sara.melo@ipmfor.ce.gov.br>
2022-06-03 16:40:14 -0300

    logo configurada, barra de busca e espaco dos pokemons add

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -7,8 +7,11 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { StatusComponent } from './status/status.component';
 
-// Rotas
+// Routes
 import { RoutingModule } from './routing.module';
+
+// Modules
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -17,7 +20,8 @@ import { RoutingModule } from './routing.module';
   ],
   imports: [
     CommonModule,
-    RoutingModule
+    RoutingModule,
+    SharedModule
   ]
 })
 
