6ab584ba573c62ee4cef851ac1723df1b5401a03
sara.melo <sara.melo@ipmfor.ce.gov.br>
2022-06-08 14:53:58 -0300

    puxando da api info dos cards, css todo bugado tem q ajeitar saporra

--- src/app/pages/status/status.component.ts
+++ src/app/pages/status/status.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+
+import { ApiService } from './../../service/api.service';
+
 
 @Component({
   selector: 'app-status',
   templateUrl: './status.component.html',
   styleUrls: ['./status.component.scss']
 })
+
 export class StatusComponent implements OnInit {
 
-  constructor() { }
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
+
+  public pokemon: any;
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private apiService: ApiService ) { }
 
   ngOnInit(): void {
+    this.pokemonId;
+  }
+
+  get pokemonId() {
+    const id = this.activatedRoute.snapshot.params['id'];
+    const pokemon = this.apiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
+    const name = this.apiService.apiGetPokemons(`${this.urlName}/${id}`);
+
+    return forkJoin([pokemon, name]).subscribe(
+      response => {
+        this.pokemon = response;
+      }
+    );
+
+    //return console.log(id);
   }
 
 }
